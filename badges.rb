1636496c9247167e736edf52150c00d08d574fa5
Brooke Jones <bjones782@gmail.com>
2019-02-17 21:04:18 +0000

    Automatically backed up by Learn

--- badges.rb
+++ badges.rb
@@ -5,16 +5,31 @@ end
 speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(names)
-  array = [] 
- speaker_names.each do |message|
-   array = badge_maker(message)
-   speaker_names.push(badge_maker)
-   end
-  array
+  new_array = []
+  names.each do |name|
+   new_array.push("Hello, my name is #{name}.")
+end
+  new_array
 end
 
-def assign_rooms(speaker_names)
+ def assign_rooms(arr)
+  new_array = []
+  counter = 1
+  arr.each do |name|
+  new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  counter+=1
+  end
+  return new_array  
+end
+
+ def printer(arr)
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
+
+   assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
+end
   
-  
-  
-  
+  # I know that this is not complete or working yet. THANK YOU so much for this opportunity!!! I cannot explain how much I appreciate the chance to learn with you this weekend! I am brand new to coding and know that I have a TON to learn! I am completely dedicated to doing what it takes! 
